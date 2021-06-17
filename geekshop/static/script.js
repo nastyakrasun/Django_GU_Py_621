04708cbf5bedfe09875802dcb7a48228b2d8493c
Nastya <nastyakarsun@gmail.com>
2021-06-17 16:43:18 +0300

    не считает итог при изменении количества позиций в корзине

--- geekshop/static/script.js
+++ geekshop/static/script.js
@@ -1,8 +1,8 @@
-windows.onload = function() {
+window.onload = function() {
 	$('.basket_list').on( types: 'click', selector: 'input[type="number"]', data: function() {
 		var t_href = event.target;
 		$ajax( url: {
-			url: "/basket/edit/""+ t_href + "/" + t_href.value + "/",
+			url: "/basket/edit/" + t_href.name + "/" + t_href.value + "/",
 
 			success:  function(data) {
  				$('.basket_list').html(data.result);
